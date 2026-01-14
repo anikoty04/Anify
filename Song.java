549784f7a060dbf6f847a3c29c50b940a3a2cd32
Telemanton <156657205+Telemanton@users.noreply.github.com>
2026-01-15 00:23:31 +0100

    Correción 15/01/2026

--- Song.java
+++ Song.java
@@ -1,11 +1,15 @@
 public class Song {
     //Declare the variables that a song can have
+
+    // Correction: Need to explain what the variables will be used for
     private String title;
-    private String artist;
-    private float duration;
-    private int playback;
+    // Coulde be artist , artitsts , group , band , etc
+    private String artist; 
+    private float duration; // Why float? 
+    private int playback; // How will you use playback? Consider renaming for clarity
     private boolean explicitContent;
-    //Make the main constructor with the exceptions
+    //Makes the main constructor with exceptions
+    
     public Song (String title, String artist, int duration, int playback, boolean explicitContent) {
         if (title == null || title.isBlank()) {
             throw new IllegalArgumentException ("A title is needed for the song");
@@ -13,16 +17,18 @@ public class Song {
         if (artist == null || artist.isBlank()) {
             throw new IllegalArgumentException ("There must be an assigned artist");
         }
-        if (duration < 0) {
-            throw new IllegalArgumentException("The duration cant be negative");
+        if (duration < 0) { // Correction: duration must not be 0 too , so change to <= 0
+            throw new IllegalArgumentException("Duration cannot be negative");
         }
         if (playback < 0) {
-            throw new IllegalArgumentException("The playback cannot be negative");
+            throw new IllegalArgumentException("Playback cannot be negative");
         }
         this.artist = artist;
         this.title = title;
         this.playback = playback;
         this.duration = duration;
-        this.explicitContent = explicitContent;
+        this.explicitContent = explicitContent; 
     }
+
+    //Getters for all the variables are needed, use try/catch if necessary
 }
